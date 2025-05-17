ebb6827dc81c34ea8e8ea5d80c851ddf552a512e
lorrisc <l.crappier@gmail.com>
2025-05-17 11:42:18 +0200

    23-d1-2

--- 2023/day01/main.py
+++ 2023/day01/main.py
@@ -1,5 +1,6 @@
 import sys
 from pathlib import Path
+import re
 
 # Ajouter le chemin racine du projet au PYTHONPATH
 sys.path.append(str(Path(__file__).resolve().parent.parent.parent))
@@ -11,29 +12,59 @@ from tools.fct_file import *
 content = read_file(Path(__file__).parent / "input.txt", True)
 lignes = content.split("\n")
 
-calibs = []
 
-for ligne in lignes:
+def part1(lignes):
+    calibs = []
 
-    first_dgt = None
-    last_dgt = None
+    for ligne in lignes:
 
-    for char in ligne:
-        if char.isdigit():
-            first_dgt = char
-            break
+        first_dgt = None
+        last_dgt = None
 
-    for char in reversed(ligne):
-        if char.isdigit():
-            last_dgt = char
-            break
+        for char in ligne:
+            if char.isdigit():
+                first_dgt = char
+                break
 
-    calib_val = first_dgt + last_dgt
+        for char in reversed(ligne):
+            if char.isdigit():
+                last_dgt = char
+                break
 
-    calibs.append(int(calib_val))
+        calib_val = first_dgt + last_dgt
 
-calibs_sum = 0
-for val in calibs:
-    calibs_sum += val
+        calibs.append(int(calib_val))
 
-print(calibs_sum)
+    calibs_sum = sum(calibs)
+
+    print("PARTIE 1 : ")
+    print(calibs_sum)
+
+def part2(lignes):
+    numbers_str = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+    calibs = []
+
+
+    for ligne in lignes:
+        all_find = {}
+        for idx, char in enumerate(ligne):
+            if char.isdigit():
+                all_find[idx] = char
+
+        for idx, char in enumerate(numbers_str):
+            matches = re.finditer(char, ligne)
+
+            for match in matches:
+                all_find[match.start()] = idx + 1
+
+        minval = all_find[min(all_find)]
+        maxval = all_find[max(all_find)]
+        calibs.append(int(str(minval) + str(maxval)))
+
+    calibs_sum = sum(calibs)
+
+    print("PARTIE 2 : ")
+    print(calibs_sum)
+
+part1(lignes)
+part2(lignes)
