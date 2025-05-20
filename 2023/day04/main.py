f6dca2b38c4ef9682644dc3a31dbfc77db65f5d2
lorrisc <l.crappier@gmail.com>
2025-05-20 20:48:23 +0200

    23-d4-2 + refactor

--- 2023/day04/main.py
+++ 2023/day04/main.py
@@ -8,22 +8,48 @@ sys.path.append(str(Path(__file__).resolve().parent.parent.parent))
 # Importer toutes les fonctions ou variables de fct_file
 from tools.fct_file import *
 
-
 content = read_file(Path(__file__).parent / "input.txt", True)
 cards = content.split('\n')
-cards = [card.split(": ")[1].split(" | ") for card in cards]
 
-win_score = 0
+def part1(cards):
+    cards = [card.split(": ")[1].split(" | ") for card in cards]
 
-for card in cards:
-    win_numbers = card[0].split()
-    card_numbers = card[1].split()
-    win_occurences = 0
+    win_score = 0
 
-    for win_number in win_numbers:
-        if win_number in card_numbers:
-            win_occurences += 1
+    for card in cards:
+        win = set(card[0].split())
+        have = set(card[1].split())
+        
+        matches = len(win & have)
 
-    win_score += 2 ** (win_occurences - 1) if win_occurences > 0 else 0
+        win_score += 2 ** (matches - 1) if matches > 0 else 0
 
-print(win_score)
+    print(f"PARTIE 1 : {win_score}")
+
+part1(cards)
+
+def part2(cards):
+    nb_card_instances = {}
+    for i in range(1, len(cards) + 1):
+        nb_card_instances[i] = 1
+
+    for card in cards:
+
+        card_num, card_data = card.split(": ")
+        card_num = int(card_num.split()[1])
+        win, have = card_data.split(" | ")
+
+        win = set(win.split())
+        have = set(have.split())
+        
+        matches = len(win & have)
+
+        for i in range(1, matches + 1):
+            nb_card_instances[card_num + i] += nb_card_instances[card_num]
+
+    nb_tot_cards = sum(nb_card_instances.values())
+
+    print(f"PARTIE 2 : {nb_tot_cards}")
+        
+part2(cards)
+    
